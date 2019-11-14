3a153e7500babbc5604d32257a9679b3a895c54e
BartoroDev <57769923+BartoroDev@users.noreply.github.com>
2019-11-14 22:45:55 +0100

    zmiana cudzysłowiów

--- main.c
+++ main.c
@@ -7,17 +7,17 @@ for(int i=0;i<gra->rozmiar;i++){
     for(int i=0;i<3;i++){
         for(int j=0;j<gra->rozmiar;j++){
             if((i+j)%2==0)
-            gra->plansza[i][j]=" ";
+            gra->plansza[i][j]=' ';
             else
-           gra->plansza[i][j]="X";           //zapisywanie pierwszych trzech wierszy X-ami
+           gra->plansza[i][j]='X';           //zapisywanie pierwszych trzech wierszy X-ami
         }
     }
     for(int i=(gra->rozmiar-3);i<gra->rozmiar;i++){
         for(int j=0;j<gra->rozmiar;j++){
             if((i+j)%2==0)
-            gra->plansza[i][j]=" ";
+            gra->plansza[i][j]=' ';
             else
-           gra->plansza[i][j]="O";     //zapisywanie ostatnich trzech wierszy O-ami
+           gra->plansza[i][j]='O';     //zapisywanie ostatnich trzech wierszy O-ami
         }
     }
     for(int i = 0; i < gra->rozmiar; i++,printf("\n"))
